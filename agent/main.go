d2ff09e42032e5013ee2feb0d040acfa9f1ed29a
Vahn Gomes <me@vahngomes.dev>
2024-01-14 20:40:03 -0500

    Add Dockerfile and compose.yaml for agent service

--- agent/main.go
+++ agent/main.go
@@ -7,8 +7,8 @@ import (
 
 // Array of required environment variables
 var requiredEnvVars = []string{
-	"SERVER_URL",
-	"AGENT_TOKEN",
+	"AGENT_SERVER_URL",
+	"AGENT_SECRET",
 }
 
 func main() {
@@ -34,14 +34,18 @@ func main() {
 	}
 
 	// perform check to ensure the server is healthy and ready to accept connections
-	if !checkServerHealth(os.Getenv("SERVER_URL")) {
+	if !checkServerHealth(os.Getenv("AGENT_SERVER_URL")) {
 		log.Error("agent", "Server is not healthy")
 		return
 	}
 
 	// perform E2E encryption handshake with the server
 
-	// begin polling the server for work
+	// if the agent is being initialized, send the agent token to the server
+	// once authenticated, the server will send a public key to encrypt traffic with
+	// the agent will then send a public key to the server to encrypt traffic with the agent
+
+	// After the handshake, the agent will begin polling the server for containers to monitor
 
 	// if the server returns or changes the needed container name, add it to the list of containers to monitor
 
@@ -58,9 +62,5 @@ func checkServerHealth(url string) bool {
 	}
 	defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
-		return false // return false if unhealthy
-	}
-
-	return true // return true if healthy
+	return resp.StatusCode == http.StatusOK // return true if healthy
 }
