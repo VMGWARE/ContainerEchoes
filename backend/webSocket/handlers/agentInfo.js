3f7a2562088b096bd9da1d47f3d6374b48eefe6a
Insidious Fiddler <me@vahngomes.dev>
2024-01-23 15:56:10 -0500

    Feature: event driven ws handling (#26)
    * Refactor WebSocket handlers and add handshake functionality
    
    * Refactor WebSocket message handling and handshake process
    
    * Add logging to WebSocket message handling
    
    * Update handle() method in MessageHandlerBase

---
+++ backend/webSocket/handlers/agentInfo.js
@@ -0,0 +1,89 @@
+const MessageHandlerBase = require("./messageHandlerBase");
+const knex = require("@container-echoes/core/database");
+const config = require("@container-echoes/core/config").getInstance();
+const rsa = require("trsa");
+const log = require("@vmgware/js-logger").getInstance();
+
+/**
+ * Represents a handler for the handshake process with agents.
+ * @extends MessageHandlerBase
+ */
+class HandleHandshake extends MessageHandlerBase {
+	/**
+	 * Creates an instance of HandleHandshake.
+	 * @param {WebSocketManager} webSocketManager - The WebSocket manager instance.
+	 */
+	constructor(webSocketManager) {
+		super(webSocketManager, "agentInfo");
+	}
+
+	/**
+	 * Handles the handshake message from the agent.
+	 * Decrypts the message data, checks agent validity, and sends a response.
+	 * @param {WebSocket} ws - The WebSocket connection instance.
+	 * @param {Object} messageObj - The received message object.
+	 * @throws {Error} Throws an error if there is an issue decrypting the message data.
+	 * @returns {Promise<void>} A Promise that resolves when the handling is complete.
+	 */
+	async handle(ws, messageObj) {
+		messageObj.data = JSON.parse(
+			rsa.decrypt(messageObj.data, this.webSocketManager.server.privateKey)
+		);
+
+		let token = messageObj.data.token;
+		let hostname = messageObj.data.hostname;
+
+		// Check if the agent is valid
+		let agent = await knex("agent")
+			.where({
+				token: token,
+			})
+			.first();
+
+		if (!agent) {
+			if (config.app.autoAddAgents) {
+				log.debug(
+					"WebSocketManager",
+					"Agent is not valid, but auto add is enabled"
+				);
+
+				// Add the agent to the database
+				await knex("agent").insert({
+					token: token,
+					publickey: ws.publicKey,
+					hostname: hostname,
+				});
+
+				// Get the agent's info
+				agent = await knex("agent")
+					.where({
+						token: token,
+					})
+					.first();
+			} else {
+				log.debug("WebSocketManager", "Agent is not valid");
+				ws.close();
+			}
+		}
+
+		// Store the agent's id
+		ws.id = agent.agentId;
+
+		log.debug("WebSocketManager", `Authenticated agent ${agent.agentId}`);
+
+		// Send the agent's id to the agent
+		this.webSocketManager.sendMessage(
+			ws,
+			this.webSocketManager.buildMessage(
+				"ok",
+				"agentId",
+				{
+					agentId: agent.agentId,
+				},
+				true
+			)
+		);
+	}
+}
+
+module.exports = HandleHandshake;
