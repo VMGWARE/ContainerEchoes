fe495dc782c840516f8a3dec2358890b3e17178d
Vahn Gomes <me@vahngomes.dev>
2024-01-25 16:42:41 -0500

    Update server configuration and Swagger URL

--- server/configs/swagger.js
+++ server/configs/swagger.js
@@ -1,9 +1,12 @@
 const { getVersion } = require("../utils/general");
 const path = require("path");
+const config = require("@container-echoes/core/config").getInstance();
 
 const currentPath = path.dirname(__filename);
 const controllersPath = path.join(currentPath, "../controllers");
 const routesPath = path.join(currentPath, "../routes");
+
+const url = config.app.url;
 
 const options = {
 	definition: {
@@ -16,12 +19,7 @@ const options = {
 		},
 		servers: [
 			{
-				url: "https://echoes.vmgware.dev/api",
-				description: "Production server (uses live data)",
-			},
-			{
-				url: "http://localhost:5000",
-				description: "Development server (uses test data)",
+				url,
 			},
 		],
 	},
