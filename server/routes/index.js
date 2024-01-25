daa645501a9621df3e44e19fd6f94e0a21f43b3a
Vahn Gomes <me@vahngomes.dev>
2024-01-25 16:34:50 -0500

    Fix route loading issues

--- server/routes/index.js
+++ server/routes/index.js
@@ -34,18 +34,39 @@ const routeFiles = fs
 	.filter((file) => file.endsWith(".js") && file !== path.basename(__filename));
 
 const routes = {};
+let issue = false;
 
 routeFiles.forEach((file) => {
 	// If the file is this file, skip it
+	// This is just a safety check
 	if (file === path.basename(__filename)) {
 		return;
 	}
 
-	const route = require(`./${file}`);
-	const routeName = file.replace(".js", "");
-	logRoutes(route, routeName);
-	routes[`${routeName}Routes`] = route;
+	try {
+		const route = require(`./${file}`);
+		const routeName = file.replace(".js", "");
+		logRoutes(route, routeName);
+		routes[`${routeName}Routes`] = route;
+	} catch (err) {
+		issue = true;
+		if (err.code === "MODULE_NOT_FOUND") {
+			log.error("routes", `Could not find route file ${file}`);
+		}
+		if (err instanceof SyntaxError) {
+			log.error("routes", `Syntax error in route file ${file}: ${err.message}`);
+		}
+		if (err instanceof TypeError) {
+			log.error("routes", `Type error in route file ${file}: ${err.message}`);
+		} else {
+			log.error("routes", `Error loading route file ${file}: ${err.message}`);
+		}
+	}
 });
+
+if (issue) {
+	log.warn("routes", "There were issues loading some routes");
+}
 
 // Export all routes
 module.exports = routes;
