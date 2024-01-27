9310a60731a9f020688c70d1c0b22ee505eff407
Vahn Gomes <me@vahngomes.dev>
2024-01-26 23:01:12 -0500

    Refactor App.vue component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <v-app>
-    <DefaultLayout v-if="!isLoading && user.loggedIn">
-      <router-view />
-    </DefaultLayout>
-    <BlankLayout v-else-if="!isLoading && !user.loggedIn">
-      <router-view />
-    </BlankLayout>
-    <div v-else class="d-flex justify-center align-center fill-height">
-      <v-progress-circular indeterminate color="primary"></v-progress-circular>
-
-      <div class="ml-2">Loading...</div>
-    </div>
-  </v-app>
+  <VApp>
+    <RouterView />
+  </VApp>
 </template>
 
 <script>
-import DefaultLayout from "./layouts/default.vue";
-import BlankLayout from "./layouts/blank.vue";
-import { useUserStore } from "./store/user";
-import { useAppStore } from "@/store/app";
-import axios from "axios";
-import { jwtDecode } from "jwt-decode";
+import { useUserStore } from './store/user'
+import { useAppStore } from '@/store/app'
+import axios from 'axios'
+import { jwtDecode } from 'jwt-decode'
 
 export default {
   data() {
@@ -28,132 +16,120 @@ export default {
       isLoading: true,
       tokenRefreshTimer: null,
       user: {},
-    };
-  },
-  components: {
-    DefaultLayout,
-    BlankLayout,
+    }
   },
   methods: {
     check_token() {
-      console.log("Checking JWT token");
-      const token = this.user.token;
+      console.log('Checking JWT token')
+      const token = this.user.token
       if (!token || token == null) {
-        console.log("No token found");
-        let redirect = window.location.pathname;
+        console.log('No token found')
+        let redirect = window.location.pathname
 
         // Don't redirect to login page if already on login page
         if (useAppStore().publicRoutes.includes(redirect)) {
-          console.log("Not redirecting to login page");
-          this.isLoading = false;
-          return;
+          console.log('Not redirecting to login page')
+          this.isLoading = false
+          return
         }
 
         // If redirect does not contain login or signup, redirect to login
         if (!useAppStore().publicRoutes.includes(redirect)) {
-          redirect = "/login?redirect=" + redirect;
+          redirect = '/login?redirect=' + redirect
         }
 
-        this.isLoading = false;
+        this.isLoading = false
         // Redirect to login page
-        this.$router.push(redirect);
-        return;
+        this.$router.push(redirect)
+        return
       }
-      let exp;
+      let exp
       try {
-        const { exp: fixAttemptExp } = jwtDecode(token);
-        exp = fixAttemptExp;
+        const { exp: fixAttemptExp } = jwtDecode(token)
+        exp = fixAttemptExp
       } catch (e) {
-        console.log("Invalid token");
-        this.isLoading = false;
-        this.logout();
-        return;
+        console.log('Invalid token')
+        this.isLoading = false
+        this.logout()
+        return
       }
-      console.log("Your JWT is", exp - Date.now() / 1000, "seconds to expiry");
+      console.log('Your JWT is', exp - Date.now() / 1000, 'seconds to expiry')
       if (exp - Date.now() / 1000 < 0) {
-        console.log("Token has expired");
-        clearTimeout(this.tokenRefreshTimer);
-        this.isLoading = false;
-        this.logout();
-        return;
+        console.log('Token has expired')
+        clearTimeout(this.tokenRefreshTimer)
+        this.isLoading = false
+        this.logout()
+        return
       }
       if (exp - Date.now() / 1000 < 600 && exp - Date.now() / 1000 > 0) {
-        axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
         axios({
-          method: "post",
-          url: "/api/auth/refresh-token",
-        }).then((response) => {
-          this.$store.commit("setToken", response.data.data.token);
-          localStorage.setItem("token", response.data.data.token);
-          console.log("token refreshed");
-        });
-        this.isLoading = false;
+          method: 'post',
+          url: '/api/auth/refresh-token',
+        }).then(response => {
+          this.$store.commit('setToken', response.data.data.token)
+          localStorage.setItem('token', response.data.data.token)
+          console.log('token refreshed')
+        })
+        this.isLoading = false
       } else {
-        this.isLoading = false;
-        console.log("token not expired yet");
-        this.setupTokenRefresh();
+        this.isLoading = false
+        console.log('token not expired yet')
+        this.setupTokenRefresh()
       }
     },
     setupTokenRefresh() {
-      console.log("Setup token refresh");
+      console.log('Setup token refresh')
 
       // Clear any existing timer before setting up a new one
-      const existingTimerId = localStorage.getItem("tokenRefreshTimer");
+      const existingTimerId = localStorage.getItem('tokenRefreshTimer')
       if (existingTimerId) {
-        console.log("Clearing existing timer", existingTimerId);
-        clearTimeout(existingTimerId);
-        localStorage.removeItem("tokenRefreshTimer");
+        console.log('Clearing existing timer', existingTimerId)
+        clearTimeout(existingTimerId)
+        localStorage.removeItem('tokenRefreshTimer')
       }
 
       // Setup new timer
-      const token = this.user.token;
-      const { exp } = jwtDecode(token);
-      const newTimerId = setTimeout(
-        this.check_token,
-        (exp - Date.now() / 1000 - 600) * 1000
-      );
+      const token = this.user.token
+      const { exp } = jwtDecode(token)
+      const newTimerId = setTimeout(this.check_token, (exp - Date.now() / 1000 - 600) * 1000)
 
       // Store new timer ID in localStorage
-      localStorage.setItem("tokenRefreshTimer", newTimerId);
+      localStorage.setItem('tokenRefreshTimer', newTimerId)
 
-      console.log(
-        "New token refresh timer set for",
-        exp - Date.now() / 1000 - 600,
-        "seconds, timerId:",
-        newTimerId
-      );
+      console.log('New token refresh timer set for', exp - Date.now() / 1000 - 600, 'seconds, timerId:', newTimerId)
     },
     logout() {
-      this.isLoading = true;
-      this.user.logout();
+      this.isLoading = true
+      this.user.logout()
 
       // Redirect to login page
-      this.isLoading = false;
-      this.$router.push("/login");
+      this.isLoading = false
+      this.$router.push('/login')
     },
   },
   mounted() {
-    this.user = useUserStore();
-    this.check_token();
+    this.user = useUserStore()
+    this.check_token()
   },
   unmounted() {
-    console.log("Remove token refresh timer");
-    const timerId = localStorage.getItem("tokenRefreshTimer");
+    console.log('Remove token refresh timer')
+    const timerId = localStorage.getItem('tokenRefreshTimer')
     if (timerId) {
-      clearTimeout(timerId);
-      localStorage.removeItem("tokenRefreshTimer");
+      clearTimeout(timerId)
+      localStorage.removeItem('tokenRefreshTimer')
     }
   },
   watch: {
     $route() {
-      this.check_token();
+      this.check_token()
     },
     useUserStore: {
       handler() {
-        this.check_token();
+        this.check_token()
       },
       deep: true,
     },
   },
-};
+}
 </script>
