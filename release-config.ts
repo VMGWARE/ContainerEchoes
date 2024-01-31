34455e6c151142465a0c850e0ffc89586a2b8cd8
Insidious Fiddler <me@vahngomes.dev>
2024-01-31 14:44:18 -0500

    Update package.json versions for server and web projects via release helper (#83)

--- release-config.ts
+++ release-config.ts
@@ -1,3 +1,11 @@
 export default {
   commentOnReleasedPullRequests: false,
+  // Update the package.json version for the server and web projects
+  beforePrepare: async ({ exec, nextVersion }) => {
+    // Update the server package.json version
+    await exec(`cd server && npm version ${nextVersion} --no-git-tag-version`);
+
+    // Update the web package.json version
+    await exec(`cd web && npm version ${nextVersion} --no-git-tag-version`);
+  },
 };
