e9be46d7808f46457cd1e099995349896209ef45
Vahn Gomes <me@vahngomes.dev>
2024-02-10 00:08:31 -0500

    Add writing of logs to db

--- server/webSocket/handlers/log.js
+++ server/webSocket/handlers/log.js
@@ -1,5 +1,6 @@
 const MessageHandlerBase = require("./messageHandlerBase");
 const rsa = require("trsa");
+const knex = require("@container-echoes/core/database");
 
 /**
  * Represents a handler for the handshake process with agents.
@@ -20,6 +21,32 @@ class HandleHandshake extends MessageHandlerBase {
 	 */
 	async handle(ws, messageObj) {
 		// console.log(messageObj); // Commented out as it dumps a lot of data to the console
+		const containerId = messageObj.data.id;
+		const logs = messageObj.data.logs;
+
+		// TODO: Switch from MySQL to PostgreSQL due to needed number of writes
+
+		// TODO: Make helper class for logs:
+		// 1. Storing of data (pg or elasticsearch if enabled)
+		// 2. Migration of data (from pg to elasticsearch if enabled)
+		// 3. Retrieval of data (from pg or elasticsearch if enabled)
+
+		// TODO: Optimize, Optimize, Optimize
+
+		// TODO: Ensure that after the logs are inserted, we let the agent know that we've received the logs and the next latest
+		// timestamp we have for the logs so that it can send us the logs from that timestamp onwards
+
+		// Insert the logs into the database
+		await knex("log").insert(
+			logs.map((log) => {
+				return {
+					container: containerId,
+					// TODO: Add a timestamp to the logs
+					// TODO: Extract the log level from the log message if it's present
+					message: log,
+				};
+			})
+		);
 	}
 
 	async preprocess(messageObj) {
